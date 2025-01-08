9c26cd88180fdca4a3bd868b4747b62a17f3603a
NataliaA707 <anisimovanatalia@icloud.com>
2025-01-09 00:47:11 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,12 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+while index < len(my_list):
+    number = my_list[index]
+    if number > 0:
+        print(number)
+    elif number == 0:
+        index += 1
+        continue
+    else:
+        break
+    index += 1
